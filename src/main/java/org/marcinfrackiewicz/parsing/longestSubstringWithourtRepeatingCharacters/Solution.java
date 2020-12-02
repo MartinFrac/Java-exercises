fe13cd8dff3a7582a16a87dd31653e166fe66fea
Marcin <marcin.jarek.frackiewicz@gmail.com>
2020-12-02 18:04:18 +0000

    longest substring without repeating characters implemented

---
+++ src/main/java/org/marcinfrackiewicz/parsing/longestSubstringWithourtRepeatingCharacters/Solution.java
@@ -0,0 +1,28 @@
+package org.marcinfrackiewicz.parsing.longestSubstringWithourtRepeatingCharacters;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+    public static String string = "gfgfgdg gfdgsdsal;mvnfsdfs";
+
+    public static void main(String[] args) {
+        System.out.println(lenghtOfLongestSubstring(string));
+    }
+
+    public static int lenghtOfLongestSubstring(String s) {
+        Set<Character> visited = new HashSet<>();
+        int n = s.length();
+        int max = 0, i = 0, j = 0;
+        while (i < n && j < n) {
+            if (!visited.contains(s.charAt(j))){
+                visited.add(s.charAt(j++));
+                max = Math.max(max, j - i);
+            }
+            else {
+                visited.remove(s.charAt(i++));
+            }
+        }
+        return max;
+    }
+}
