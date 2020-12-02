d3e4e328646fa3c3fb87661474d8dc704dab74be
Marcin <marcin.jarek.frackiewicz@gmail.com>
2020-12-02 18:00:09 +0000

    Edge class added

---
+++ src/main/java/org/marcinfrackiewicz/algorithms/Edge.java
@@ -0,0 +1,11 @@
+package org.marcinfrackiewicz.algorithms;
+
+public class Edge {
+    public int from, to, weight;
+
+    public Edge(int from, int to, int weight) {
+        this.from = from;
+        this.to = to;
+        this.weight = weight;
+    }
+}
